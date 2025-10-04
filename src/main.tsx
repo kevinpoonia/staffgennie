9cb79653fccf90aaa83f84fcc06b9ff13f892e57
Vishal Poonia <121330381+kevinpoonia@users.noreply.github.com>
2025-10-05 04:09:38 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <meta name="google-site-verification" content="1HCPSEKoBsMyA7s3u2JrVAkCsrFd0d9e11Z9GJ89T_Q" />
     <App />
   </StrictMode>,
 )
